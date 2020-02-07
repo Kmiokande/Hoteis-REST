e97d355def6e3744414910d25e928fa4bbd41c5f
Kmiokande <brunnodesouzabezerra@gmail.com>
2020-02-07 12:36:12 -0300

    Implementing POST Hotel

--- resources/hotel.py
+++ resources/hotel.py
@@ -1,36 +1,51 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 
 hoteis = [
-    {'idHotel': 'ocean',
-     'name': 'Ocean House Hotel',
-     'stars': 4.5,
-     'cost': 80.00,
-     'city': 'Santa Monica'},
-    {'idHotel': 'regente',
-     'name': 'Regente Hotel',
-     'stars': 4.8,
-     'cost': 120.00,
-     'city': 'New Orleans'}
+	{'idHotel': 'ocean',
+	 'name': 'Ocean House Hotel',
+	 'stars': 4.5,
+	 'cost': 80.00,
+	 'city': 'Santa Monica'},
+	{'idHotel': 'regente',
+	 'name': 'Regente Hotel',
+	 'stars': 4.8,
+	 'cost': 120.00,
+	 'city': 'New Orleans'}
 ]
 
 
 class Hotel(Resource):
-    def get(self, idHotel):
-        for hotel in hoteis:
-            if hotel['idHotel'] == idHotel:
-                return hotel
-        return {'message': 'Hotel not found!'}, 404
+	def get(self, idHotel):
+		for hotel in hoteis:
+			if hotel['idHotel'] == idHotel:
+				return hotel
+		return {'message': 'Hotel not found!'}, 404
 
-    def post(self, idHotel):
-        pass
+	def post(self, idHotel):
+		argumentos = reqparse.RequestParser()
+		argumentos.add_argument('name')
+		argumentos.add_argument('stars')
+		argumentos.add_argument('cost')
+		argumentos.add_argument('city')
 
-    def put(self):
-        pass
+		dados = argumentos.parse_args()
+		novoHotel = {
+			'idHotel': idHotel,
+			'name': dados['name'],
+			'stars': dados['stars'],
+			'cost': dados['cost'],
+			'city': dados['city']
+		}
+		hoteis.append(novoHotel)
+		return novoHotel, 200
 
-    def delete(self):
-        pass
+	def put(self):
+		pass
+
+	def delete(self):
+		pass
 
 
 class Hoteis(Resource):
-    def get(self):
-        return {'hoteis': hoteis}
+	def get(self):
+		return {'hoteis': hoteis}
